5d364fb5e89deb19fdc199ca016b0efd2163a031
m1lkweed <m1lkweed@users.noreply.github.com>
2023-07-26 06:09:05 -0400

    Update example.c
    Actually-working code

--- example.c
+++ example.c
@@ -5,28 +5,28 @@
 
 int main(){
 	orangey_ctx_t rng = {};
-	void orangey_srand(&rng, 0, 0);
+	orangey_srand(&rng, 0, 0);
 	for(int i = 0; i < 100; ++i){
 		printf("orangey_rand(&rng):                %.16lx\n", orangey_rand(&rng));
 	}
 	orangey_skip(&rng, -100);
 	for(int i = 0; i < 100; ++i){
-		printf("orangey_rand_range(&rng, 64, 128): %.16lx\n", orangey_rand_range(&rng, 64, 128));
+		printf("orangey_rand_range(&rng, 64, 128): %.2lx\n", orangey_rand_range(&rng, 64, 128));
 	}
 	orangey_skip(&rng, -100);
 	for(int i = 0; i < 100; ++i){
-		printf("orangey_uniform_double_01(&rng):   %.16lx\n", orangey_uniform_double_01(&rng));
+		printf("orangey_uniform_double_01(&rng):   %f\n", orangey_uniform_double_01(&rng));
 	}
 	orangey_skip(&rng, -100);
 	for(int i = 0; i < 100; ++i){
-		printf("orangey_all_doubles_01(&rng):      %.16lx\n", orangey_all_doubles_01(&rng));
+		printf("orangey_all_doubles_01(&rng):      %g\n", orangey_all_doubles_01(&rng));
 	}
 	orangey_skip(&rng, -100);
 	for(int i = 0; i < 100; ++i){
-		printf("orangey_gaussian(&rng):            %.16lx\n", orangey_gaussian(&rng));
+		printf("orangey_gaussian(&rng):            %f\n", orangey_gaussian(&rng));
 	}
 	orangey_skip(&rng, -100);
 	for(int i = 0; i < 100; ++i){
-		printf("orangey_poisson(&rng, 0.33333333): %.16lx\n", orangey_poisson(&rng, 0.33333333));
+		printf("orangey_poisson(&rng, 1.33333333): %lu\n", orangey_poisson(&rng, 1.33333333));
 	}
 }
